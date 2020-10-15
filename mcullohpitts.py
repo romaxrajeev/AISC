29d4fd063e9b631b6f93c3cdb679f90ca24f7df4
Romax Rajeev <64697574+romeo611199@users.noreply.github.com>
2020-10-15 20:41:10 +0530

    Update mcullohpitts.py

--- mcullohpitts.py
+++ mcullohpitts.py
@@ -2,26 +2,31 @@
 inputs, output = [[0,0],[0,1],[1,0],[1,1]], [0,0,0,1]
 
 #Initialize weights and bias
-w1, w2, b = 1,1,1
+w1, w2 = 0,0
 
 #Activation function
 def activation(summation):
-	if summation >= 0:
+	if summation == 2:
 		return 1
 	return 0 
+print("Initial Weight 1:",w1)
+print("Initial Weight 2:",w2)
 
 i = 0
 while True:
 	x = inputs[i][0]
 	y = inputs[i][1]
-	summation = x*w1 + y*w2 + b
+	summation = x*w1 + y*w2
 	if activation(summation) > output[i]:
-		b -= 1
+		w1 -= 1
+		w2 -= 1
 	elif activation(summation) < output[i]:
-		b += 1
+		w1 += 1
+		w2 += 1
 	if activation(summation) == output[i]:
 		i += 1
 	if i == len(output):
 		break
 	
-print(w1,w2,b)
+print("Final Weight 1:",w1)
+print("Final Weight 2:",w2)
